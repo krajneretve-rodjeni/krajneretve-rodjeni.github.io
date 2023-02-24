29b3323b928a5ca1b5faf7f27832467644ac672d
krajneretve-rodjeni <126243158+krajneretve-rodjeni@users.noreply.github.com>
2023-02-24 12:00:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const collapsibleRows = document.querySelectorAll('.collapsible-row');
 
+if (collapsibleRows.length === 0) {
+  console.error('No collapsible rows found.');
+}
+
 collapsibleRows.forEach(row => {
   row.addEventListener('click', () => {
-    row.nextElementSibling.classList.toggle('show');
+    const contentRow = row.nextElementSibling;
+    if (!contentRow || !contentRow.classList.contains('collapsible-content')) {
+      console.error('Could not find collapsible content row for clicked row.');
+      return;
+    }
+    contentRow.classList.toggle('show');
   });
 });
