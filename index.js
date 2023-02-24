428b3212774ddf81115a817c680260f7b2d9a522
krajneretve-rodjeni <126243158+krajneretve-rodjeni@users.noreply.github.com>
2023-02-24 12:41:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ rows.forEach(row => {
       // Deselect row if it's already selected
       row.classList.remove('selected');
       selectedRow = null;
+
+      // Restore original row content
+      const cells = row.querySelectorAll('td');
+      cells[0].textContent = row.dataset.date;
+      cells[1].textContent = row.dataset.opponent;
+      cells[2].textContent = row.dataset.score;
     } else {
       // Hide previously selected row and show current row
       if (selectedRow) {
         selectedRow.classList.remove('selected');
         selectedRow.classList.add('hidden');
+
+        // Restore original content of previously selected row
+        const prevCells = selectedRow.querySelectorAll('td');
+        prevCells[0].textContent = selectedRow.dataset.date;
+        prevCells[1].textContent = selectedRow.dataset.opponent;
+        prevCells[2].textContent = selectedRow.dataset.score;
       }
       row.classList.remove('hidden');
       row.classList.add('selected');
