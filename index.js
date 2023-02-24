1ac46a5a16e2b073f605910697daf07c808de7d0
krajneretve-rodjeni <126243158+krajneretve-rodjeni@users.noreply.github.com>
2023-02-24 12:58:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,82 @@
-const table = document.querySelector('table');
-const rows = table.querySelectorAll('tbody tr');
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Football Scores</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <table id="football-table">
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Opponent</th>
+        <th>Score</th>
+        <th>Result</th>
+        <th>Attendance</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="game-row" data-id="1">
+        <td>1/1/2023</td>
+        <td>Team A</td>
+        <td><span class="highlight">3-1</span></td>
+        <td>Win</td>
+        <td>10,000</td>
+      </tr>
+      <tr class="game-row" data-id="2">
+        <td>1/8/2023</td>
+        <td>Team B</td>
+        <td><span class="highlight">0-0</span></td>
+        <td>Draw</td>
+        <td>8,000</td>
+      </tr>
+      <tr class="game-row" data-id="3">
+        <td>1/15/2023</td>
+        <td>Team C</td>
+        <td><span class="highlight">2-1</span></td>
+        <td>Win</td>
+        <td>12,000</td>
+      </tr>
+      <!-- add more rows here as needed -->
+    </tbody>
+  </table>
 
-let selectedRow = null;
+  <div id="game-box">
+    <table>
+      <thead>
+        <tr>
+          <th>Team A</th>
+          <th>Score</th>
+          <th>Score</th>
+          <th>Team B</th>
+          <th>Result</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Player 1</td>
+          <td>Player 2</td>
+          <td>Player 3</td>
+          <td>Player 1</td>
+          <td rowspan="11">Win</td>
+        </tr>
+        <tr>
+          <td>Player 4</td>
+          <td>Player 5</td>
+          <td>Player 6</td>
+          <td>Player 2</td>
+        </tr>
+        <tr>
+          <td>Player 7</td>
+          <td>Player 8</td>
+          <td>Player 9</td>
+          <td>Player 3</td>
+        </tr>
+        <!-- add more rows here as needed -->
+      </tbody>
+    </table>
+  </div>
 
-rows.forEach(row => {
-  row.addEventListener('click', () => {
-    if (row === selectedRow) {
-      // Deselect row if it's already selected
-      row.classList.remove('selected');
-      selectedRow = null;
-
-      // Restore original row content
-      const cells = row.querySelectorAll('td');
-      cells[0].textContent = row.dataset.date;
-      cells[1].textContent = row.dataset.opponent;
-      cells[2].textContent = row.dataset.score;
-    } else {
-      // Hide previously selected row and show current row
-      if (selectedRow) {
-        selectedRow.classList.remove('selected');
-        selectedRow.classList.add('hidden');
-
-        // Restore original content of previously selected row
-        const prevCells = selectedRow.querySelectorAll('td');
-        prevCells[0].textContent = selectedRow.dataset.date;
-        prevCells[1].textContent = selectedRow.dataset.opponent;
-        prevCells[2].textContent = selectedRow.dataset.score;
-      }
-      row.classList.remove('hidden');
-      row.classList.add('selected');
-
-      // Replace row content with players' names
-      const cells = row.querySelectorAll('td');
-      cells[0].textContent = 'Player no.1';
-      cells[1].textContent = 'Player no.2';
-      cells[2].textContent = 'Player no.3';
-
-      selectedRow = row;
-    }
-  });
-});
+  <script src="script.js"></script>
+</body>
+</html>
