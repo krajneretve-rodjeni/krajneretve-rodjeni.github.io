5429d2498fe9882850ea85bcc72c432c3543a2a1
krajneretve-rodjeni <126243158+krajneretve-rodjeni@users.noreply.github.com>
2023-02-24 12:38:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function showDetails(row) {
-  var detailsRow = document.getElementById("details");
-  if (detailsRow.style.display === "none") {
-    detailsRow.style.display = "table-row";
-    detailsRow.style.backgroundColor = row.style.backgroundColor;
-  } else {
-    detailsRow.style.display = "none";
-  }
-}
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tbody tr');
+
+let selectedRow = null;
+
+rows.forEach(row => {
+  row.addEventListener('click', () => {
+    if (row === selectedRow) {
+      // Deselect row if it's already selected
+      row.classList.remove('selected');
+      selectedRow = null;
+    } else {
+      // Hide previously selected row and show current row
+      if (selectedRow) {
+        selectedRow.classList.remove('selected');
+        selectedRow.classList.add('hidden');
+      }
+      row.classList.remove('hidden');
+      row.classList.add('selected');
+
+      // Replace row content with players' names
+      const cells = row.querySelectorAll('td');
+      cells[0].textContent = 'Player no.1';
+      cells[1].textContent = 'Player no.2';
+      cells[2].textContent = 'Player no.3';
+
+      selectedRow = row;
+    }
+  });
+});
